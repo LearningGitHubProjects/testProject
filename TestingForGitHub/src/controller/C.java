34288fc8fec7bac2d0ccb9f719b960c50b918fdc
samba667382 <73534473+samba667382@users.noreply.github.com>
2021-02-09 15:36:58 -0500

    changes testing

--- TestingForGitHub/src/controller/C.java
+++ TestingForGitHub/src/controller/C.java
@@ -19,14 +19,10 @@ public class C {
 		System.out.println("there is changes simultaneously.......");
 
 		System.out.println("this is testing 5 fro github");
-//>>>>>>> branch 'master' of https://github.com/LearningGitHubProjects/testProject.git
-	
-	
-<<<<<<< HEAD
+
 	System.out.println("Origin changes done.......");
-=======
+
 	System.out.println("THEoeE CHANGES FROM ORIGIN.......");
->>>>>>> branch 'master' of https://github.com/LearningGitHubProjects/testProject.git
 	
 	System.out.println("New statement for printing...");
 	
