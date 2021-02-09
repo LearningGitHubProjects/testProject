9d2792baf9afc399df7942e4e47b7b6b41cf54b4
LearningGitHubProjects <78783949+LearningGitHubProjects@users.noreply.github.com>
2021-02-09 14:19:16 +0530

    changes take

--- TestingForGitHub/src/controller/C.java
+++ TestingForGitHub/src/controller/C.java
@@ -1,10 +1,5 @@
 package controller;
 
 public class C {
-	
-	public void makeGitChanges()
-	{
-		System.out.println("This is changes for testing");
-		System.out.println("This is changes for testing2");
-	}
+
 }
